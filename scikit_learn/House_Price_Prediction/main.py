22e9a0bae256eb115f1abe4db403afff297c2034
Sri Lekha <srilekha.ejnavarjala03@gmail.com>
2026-05-03 20:27:53 +0530

    Updated Changes

--- scikit_learn/House_Price_Prediction/main.py
+++ scikit_learn/House_Price_Prediction/main.py
@@ -38,11 +38,24 @@ X_train = sc.fit_transform(X_train)
 #Transform test data
 X_test = sc.transform(X_test)
 
-'''
-=================================================================================================================================
-1. Implementing Linear Regression Algorithm
-=================================================================================================================================
-'''
+# Convert back to DataFrame for making code look clean and neat
+#This is mainly used for XGBoost and AdaBoost 
+X_train = pd.DataFrame(X_train, columns=[
+    "bedrooms","bathrooms","sqft_living","sqft_lot",
+    "floors","condition","grade","sqft_basement",
+    "yr_built","yr_renovated"
+])
+
+X_test = pd.DataFrame(X_test, columns=[
+    "bedrooms","bathrooms","sqft_living","sqft_lot",
+    "floors","condition","grade","sqft_basement",
+    "yr_built","yr_renovated"
+])
+
+#=================================================================================================================================
+#1. Implementing Linear Regression Algorithm
+#=================================================================================================================================
+
 from sklearn.linear_model import LinearRegression
 model = LinearRegression()
 print(model)
@@ -57,11 +70,11 @@ print("MAE:", mean_absolute_error(y_test, y_pred))
 print("MSE:", mean_squared_error(y_test, y_pred))
 print("R2 Score:", r2_score(y_test, y_pred))
 
-'''
-=================================================================================================================================
-2. Implementing Ridge Regression Algorithm
-=================================================================================================================================
-'''
+
+#=================================================================================================================================
+#12. Implementing Ridge Regression Algorithm
+#=================================================================================================================================
+
 from sklearn.linear_model import Ridge
 model = Ridge()
 print(model)
@@ -75,11 +88,11 @@ print("MAE:", mean_absolute_error(y_test, y_pred))
 print("MSE:", mean_squared_error(y_test, y_pred))
 print("R2 Score:", r2_score(y_test, y_pred))
 
-'''
-=================================================================================================================================
-3. Implementing Decision Tree Regressor
-=================================================================================================================================
-'''
+
+#=================================================================================================================================
+#3. Implementing Decision Tree Regressor
+#=================================================================================================================================
+
 
 from sklearn.tree import DecisionTreeRegressor
 model = DecisionTreeRegressor()
@@ -94,11 +107,11 @@ print("MAE:", mean_absolute_error(y_test, y_pred))
 print("MSE:", mean_squared_error(y_test, y_pred))
 print("R2 Score:", r2_score(y_test, y_pred))
 
-'''
-=================================================================================================================================
-4. Implementing Random Forest Regressor
-=================================================================================================================================
-'''
+
+#=================================================================================================================================
+#4. Implementing Random Forest Regressor
+#=================================================================================================================================
+
 from sklearn.ensemble import RandomForestRegressor
 model = RandomForestRegressor()
 print(model)
@@ -112,11 +125,11 @@ print("MAE:", mean_absolute_error(y_test, y_pred))
 print("MSE:", mean_squared_error(y_test, y_pred))
 print("R2 Score:", r2_score(y_test, y_pred))
 
-'''
-=================================================================================================================================
-5. Implementing Support Vector Regressor
-=================================================================================================================================
-'''
+
+#=================================================================================================================================
+#5. Implementing Support Vector Regressor
+#=================================================================================================================================
+
 from sklearn.svm import SVR
 model = SVR()
 print(model)
@@ -130,11 +143,11 @@ print("MAE:", mean_absolute_error(y_test, y_pred))
 print("MSE:", mean_squared_error(y_test, y_pred))
 print("R2 Score:", r2_score(y_test, y_pred))
 
-'''
-=================================================================================================================================
-6. Implementing KNN Regressor
-=================================================================================================================================
-'''
+
+#=================================================================================================================================
+#6. Implementing KNN Regressor
+#=================================================================================================================================
+
 from sklearn.neighbors import KNeighborsRegressor
 model = KNeighborsRegressor()
 print(model)
@@ -147,11 +160,11 @@ print("KNN Performance:")
 print("MAE:", mean_absolute_error(y_test, y_pred))
 print("MSE:", mean_squared_error(y_test, y_pred))
 print("R2 Score:", r2_score(y_test, y_pred))
-'''
-=================================================================================================================================
-7. Implementing Gradient Boost Regressor
-=================================================================================================================================
-'''
+
+#=================================================================================================================================
+#7. Implementing Gradient Boost Regressor
+#=================================================================================================================================
+
 from sklearn.ensemble import GradientBoostingRegressor
 model = GradientBoostingRegressor()
 print(model)
@@ -164,3 +177,89 @@ print("Gradient Boosting Performance:")
 print("MAE:", mean_absolute_error(y_test, y_pred))
 print("MSE:", mean_squared_error(y_test, y_pred))
 print("R2 Score:", r2_score(y_test, y_pred))
+
+
+#=================================================================================================================================
+#8. Implementing ElasticNet Regression
+#=================================================================================================================================
+
+from sklearn.linear_model import ElasticNet
+model = ElasticNet()
+print(model)
+
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+print('-'*80)
+print("ElasticNet Performance:")
+print("MAE:",mean_absolute_error(y_test, y_pred))
+
+
+#=================================================================================================================================
+#9. Implementing AdaBoost Regressor
+#=================================================================================================================================
+
+from sklearn.ensemble import AdaBoostRegressor
+model = AdaBoostRegressor()
+print(model)
+
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+print('-'*80)
+print("AdaBoostRegressor Performance:")
+print("MAE:", mean_absolute_error(y_test, y_pred))
+
+
+#=================================================================================================================================
+#10. Implementing Extra Trees Regressor
+#=================================================================================================================================
+
+from sklearn.ensemble import ExtraTreesRegressor
+model = ExtraTreesRegressor()
+print(model)
+
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+print('-'*80)
+print("ExtraTreeRegressor Performance:")
+print("MAE:",mean_absolute_error(y_test, y_pred))
+
+
+#=================================================================================================================================
+#11. Implementing LightGBM Regressor
+#=================================================================================================================================
+
+from lightgbm import LGBMRegressor
+model = LGBMRegressor()
+print(model)
+
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+print('-'*80)
+print("LGBMRegressor Performance:")
+print("MAE:",mean_absolute_error(y_test, y_pred))
+
+#=================================================================================================================================
+#12. Implementing XGBoost Regressor
+#=================================================================================================================================
+
+from xgboost import XGBRegressor
+model = XGBRegressor()
+print(model)
+
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+print('-'*80)
+print("XGBoost Performance:")
+print("MAE:",mean_absolute_error(y_test, y_pred))
+
+'''
+#=================================================================================================================================
+                                                                    THE END
+#=================================================================================================================================
+'''
+
